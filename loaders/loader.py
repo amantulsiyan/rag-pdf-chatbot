62572936c4e263bfb6471e239c4dca531524d060
Aman Tulsiyan <amantulsiyan12@gmail.com>
2026-01-22 12:18:48 +0530

    Completed the loader function.

---
+++ loaders/loader.py
@@ -0,0 +1,17 @@
+import fitz
+
+def load_pdf(pdf_path:str)->str:
+    doc=fitz.open(pdf_path)
+    all_text=[]
+    for page_num in range(len(doc)):
+        page=doc[page_num]
+        text=page.get_text()
+
+        if text:
+            cleaned=text.strip()
+            if cleaned:
+                all_text.append(cleaned)
+    doc.close()
+    return "/n/n".join(all_text)
+text = load_pdf("hello world.pdf")
+print(text[:1000])
