39e5c474a80eaea2ad0f00e7cfe325dac12b8fec
aman-tulsiyan <aman.tulsiyan@logicnlitera.com>
2026-03-28 17:45:13 +0530

    Add configurable top_k and reranked_k_chunks parameters to app state

--- api/dependencies.py
+++ api/dependencies.py
@@ -6,4 +6,6 @@ def initialize_app(app: FastAPI):
     app.state.faiss_index = None
     app.state.bm25_index = None
     app.state.tokenised_corpus = None
+    app.state.top_k=15
+    app.state.reranked_k_chunks=5
     app.state.llm = GroqLLM()
